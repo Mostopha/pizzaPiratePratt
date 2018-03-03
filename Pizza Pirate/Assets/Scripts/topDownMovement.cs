b051e8bc0c2c1055be845a04311c17a4dca35674
Mostafa Haque <93haque@gmail.com>
2018-03-03 11:32:03 -0500

    Orthographic movement

---
+++ Pizza Pirate/Assets/Scripts/topDownMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class topDownMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+
+        
+
+        if (moveSpeed == 0) {
+            moveSpeed = 100f;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 targetVelocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+        rb.velocity = targetVelocity * moveSpeed;
+      
+    }
+}
